6d9a943b584a5f06c44b3b14acd8c9a5cfa38be2
Galip Ümit Yolcu <gumityolcu@gmail.com>
2020-02-29 16:16:11 +0300

    Generate full couplet from FST with a function call

--- Code/FST.py
+++ Code/FST.py
@@ -18,6 +18,26 @@ class FST:
         # Load from already existing file
         pass
 
+    def generate(self):
+        state = 0
+        strr = ""
+        while state < fst.states:
+            r = random.randint(0, len(fst.machine[state]) - 1)
+            word, step = fst.machine[state][r]
+            strr += fst.words[word][0]+" "
+            state = state + step
+            if step == 0:
+                state = state + 1
+        return strr
+
+    def format(self, str):
+        str=str.replace(" <endLine> ","\n")
+        str=str.replace("<endCouplet> ","")
+        return str
+
+    def formatted(self):
+        return self.format(self.generate())
+
     def __init__(self, metre, wordsPath):
         self.errThreshold = 0
         self.metre = metre
@@ -28,11 +48,16 @@ class FST:
         for x in plainWords:
             x = x.strip()
             self.words.append((x, syllabliser.get_aruz(x)))
+        self.words.append(("<endLine>",[]))
+        endLine=len(self.words)-1
+        self.words.append(("<endCouplet>",[]))
+        endCouplet=len(self.words)-1
 
         self.lineLength = len(metre)
-        self.rootStates = [[] for i in range(self.lineLength+1)]
+        self.machine = [[] for i in range(self.lineLength+1)]
+        self.machine[self.lineLength].append((endLine,1))
 
-        for i in range(0, len(self.words)):
+        for i in range(0, len(self.words)-2):#Do not iterate over the <endCouplet> and <endLine> tokens
             (wordd, met) = self.words[i]
             longMetre = []
             for a in met:
@@ -42,22 +67,17 @@ class FST:
                 else:
                     longMetre.append(a)
 
-            for s in range(0, len(self.rootStates) - 1):
+            for s in range(0, len(self.machine) - 1):
                 if not s+len(longMetre)>len(self.metre):
                     if not computeErr(self.metre[s:s+len(longMetre)], longMetre)>self.errThreshold:
-                        self.rootStates[s].append((i, len(longMetre)))
+                        self.machine[s].append((i, len(longMetre)))
+        self.machine+=self.machine
+        self.machine.append([(endCouplet,0)])
+        self.states=len(self.machine)
 
-
-mefulumefailu=["-","-",".",".","-","-",".",".","-","-",".",".","-","-"]
-failatunfailatun=["-",".","-","-","-",".","-","-","-",".","-","-","-",".","-"]
+mefulumefailu = ["-","-",".",".","-","-",".",".","-","-",".",".","-","-"]
+failatunfailatun = ["-",".","-","-","-",".","-","-","-",".","-","-","-",".","-"]
 vezn=failatunfailatun
 fst = FST(vezn, "./revani-words")
 for arse in range(0,15):
-    state=0
-    strr=""
-    while state<len(vezn):
-        r=random.randint(0,len(fst.rootStates[state])-1)
-        word,step=fst.rootStates[state][r]
-        strr+=fst.words[word][0]+" "
-        state=state+step
-    print(strr)
+    print(fst.format(fst.generate()))
