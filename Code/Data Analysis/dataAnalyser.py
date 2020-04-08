544059a83023368098ecd898e3592322945dcc4f
rehrev <gumityolcu@gmail.com>
2020-04-08 19:25:48 +0300

    New data files

---
+++ Code/Data Analysis/dataAnalyser.py
@@ -0,0 +1,20 @@
+def OTAPAnalyser():
+    f = open("./data/mihri")
+    lines = f.readlines()
+    f.close()
+    extent = 0
+    for l in lines:
+        cont = True
+        l = l.strip()
+        if extent == 0:
+            if l == "4":
+                extent = 4
+                cont = False
+            elif l == "5":
+                extent = 5
+                cont = False
+            else:
+                extent = 2
+        if cont:
+            print(l.lower())
+            extent = extent - 1
